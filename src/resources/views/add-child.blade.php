39fa2a10b54b51c25818aed92bc75918b516fb3a
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-05-12 11:41:50 +0700

    update clid opd form
     On branch ui
     Changes to be committed:
    	modified:   src/resources/views/add-child.blade.php
    	modified:   src/resources/views/create-child.blade.php

--- src/resources/views/add-child.blade.php
+++ src/resources/views/add-child.blade.php
@@ -1,68 +1,78 @@
-<form action="{{route('storeChild',$unit_kerja->id)}}" method="post">
+@extends('master')
+@section('content')
 
-    <table>
-    
-        <tr>
-            <td><b>Root</b></td>
-        </tr>
-        <tr>
-            <td>Root</td>
-            <td>:</td>
-            <td>  
+<div class="container-fluid">
+  <div class="animated fadeIn">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1>Add New OPD</h1>
+        <hr>
+        <div class="card">
+          <div class="card-header">
+            Create <strong>Child OPD</strong>
+        </div>
+        <div class="card-body">
+
+            <form action="{{route('opd.store.child',$unit_kerja->id)}}" method="post">
+
+              <div class="form-group">
+                <label for="name">Induk Unit Kerja</label>
                 {{ $unit_kerja->kunker }} - {{ $unit_kerja->name }}
                 <input type="hidden" name="root" value="{{ $unit_kerja->id }}" >
-            </td>            
-        </tr>
-            <td><b>Children</b></td>
-        </tr>
-        <tr>
-            <td>Kode Unit Kerja</td>
-            <td>:</td>
-            <td><input type="text" name="c_kunker"></td>            
-        </tr>
-        <tr>
-            <td>Nama Unit Kerja</td>
-            <td>:</td>
-            <td><input type="text" name="c_name"></td>            
-        </tr>    
-        <tr>
-            <td>Level Unit Kerja</td>
-            <td>:</td>
-            <td>
-                <select name='c_levelunker'>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </td>            
-        </tr>    
-        <tr>
-            <td>Nama Jabatan</td>
-            <td>:</td>
-            <td><input type="text" name="c_njab"></td>            
-        </tr>
-        <tr>
-            <td>Nama Pejabat</td>
-            <td>:</td>
-            <td><input type="text" name="c_npej"></td>            
-        </tr>
-        <tr>
-            <td>Kode Unit Kerja Simral</td>
-            <td>:</td>
-            <td><input type="text" name="c_kunker_simral"></td>            
-        </tr>
-        <tr>
-            <td>Kode Unit Kerja Sinjab</td>
-            <td>:</td>
-            <td><input type="text" name="c_kunker_sinjab"></td>            
-        </tr> 
+            </div>
 
-    </table>
-    
+            <div class="form-group">
+                <label for="c_kunker">Kode Unit Kerja</label>
+                <input type="text" name="c_kunker" class="form-control" id="c_kunker" placeholder="Enter Kode Unit Kerja">
+            </div>
 
-    <button type="submit">Submit</button>
-    {{ csrf_field() }}
-    
-</form>
+            <div class="form-group">
+                <label for="c_name">Nama Unit Kerja</label>
+                <input type="text" name="c_name" class="form-control" id="c_name" placeholder="Enter Nama Unit Kerja">
+            </div>
+
+            <div class="form-group">
+              <label for="c_levelunker">Level Unit Kerja</label>
+              <select id="c_levelunker" name="c_levelunker" class="form-control form-control-lg">
+                <option value="0">Please select</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label for="c_njab">Nama Jabatan</label>
+              <input type="text" name="c_njab" class="form-control" id="c_njab" placeholder="Enter Nama Unit Kerja">
+            </div>
+
+            <div class="form-group">
+              <label for="c_npej">Nama Pejabat</label>
+              <input type="text" name="c_npej" class="form-control" id="c_npej" placeholder="Enter Nama Pejabat">
+            </div>
+
+            <div class="form-group">
+              <label for="c_kunker_simral">Kode Unit Kerja Simral</label>
+              <input type="text" name="c_kunker_simral" class="form-control" id="c_kunker_simral" placeholder="Enter Kode Unit Kerja Simral">
+            </div>
+
+            <div class="form-group">
+              <label for="c_kunker_sinjab">Kode Unit Kerja Simral</label>
+              <input type="text" name="c_kunker_sinjab" class="form-control" id="c_kunker_sinjab" placeholder="Enter Kode Unit Kerja Sinjab">
+            </div>
+
+            <hr />
+            <button type="submit" class="btn btn-primary">Submit</button>
+            {{ csrf_field() }}
+
+        </form>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+@endsection
